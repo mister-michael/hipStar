b7f6a78835754ae7447a4d93be9fe2e260242c7f
Michael Clark <michaelclarknashville@protonmail.com>
2020-03-17 14:18:03 -0500

    installed react strap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from "react-router-dom"
 import HipStar from "./components/HipStar"
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render(
