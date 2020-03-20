dc35fa4a447e0e949b89b5f722962378d2b2c0fd
Michael Clark <michaelclarknashville@protonmail.com>
2020-03-20 13:02:37 -0500

    about to fuck shit up

--- src/components/rec/RecList.js
+++ src/components/rec/RecList.js
@@ -4,40 +4,53 @@ import LoveHates from "../profile/LoveHates";
 
 const RecList = (props) => {
 
-  const activeId  = parseInt(props.userId)
+  const activeId = parseInt(props.userId)
 
   const [activeHate, setActiveHate] = useState([]);
   const [overallHate, setOverallHate] = useState([])
-  
+
 
   const recEngine = () => {
     const activeUserHate = () => {
-    return jAPI.userMovieExpand("loveHates", activeId)
-      .then(userLoveHates => {
-        const lh = userLoveHates.filter(element => element.isHated === true)
-        // const hatedMovies = userLoveHates.map(el => el.movieId)
-        setActiveHate(lh)
-      })
-  };
+      return jAPI.userMovieExpand("loveHates", activeId)
+        .then(userLoveHates => {
+          const lh = userLoveHates.filter(element => element.isHated === true)
+          // const hatedMovies = userLoveHates.map(el => el.movieId)
+          setActiveHate(lh)
+        })
+    };
 
-  activeUserHate();
+    activeUserHate();
 
-  const overallUserHate = () => {
-    return jAPI.movieExpand("loveHates")
-    .then(overallLoveHates => {
-      const olh = overallLoveHates.filter(element => element.userId !== activeId && element.isHated === true);
-      // const olhFilter = olh.filter(objects => {
-      //   for (let i =0 ; i < activeHate.length; i++) {
-      //     const sameMovies = objects.filter(object => object.movieId === activeHate[i].movieId)
-      //     return sameMovies
-      //   }
-      // })
-      setOverallHate(olh);
-    })
-  }
+    const overallUserHate = () => {
+      return jAPI.movieExpand("loveHates")
+        .then(overallLoveHates => {
+          const olh = overallLoveHates.filter(element => element.userId !== activeId && element.isHated === true);
+          const userInCommon = []
+          olh.forEach(loveHate => {
+            if (userInCommon.filter(userObj => userObj.userId !== null).length > 1) {
+              userInCommon.push({userId: loveHate.userId, hateCount: 0})
+            }
+          })
+          console.log("userincommon", userInCommon)
+          
+          setOverallHate(olh);
+        })
+    }
     overallUserHate();
 
-}
+    const sameMovies = () => {
+      let sameMovies = [];
+      for (let i = 0; i < activeHate.length; i++) {
+        const thingy = overallHate.filter(object => object.movieId === activeHate[i].movieId)
+        sameMovies = sameMovies.concat(thingy)
+      }
+      setOverallHate(sameMovies)
+    };
+
+    sameMovies();
+
+  }
   console.log(activeHate, "active");
   console.log(overallHate, "overall");
 
