edc7ef2a3ec3c45a90dc49222cd566a2f745702a
Michael Clark <michaelclarknashville@protonmail.com>
2020-03-20 14:19:37 -0500

    recs filter for similar movies

--- src/components/rec/RecList.js
+++ src/components/rec/RecList.js
@@ -11,44 +11,57 @@ const RecList = (props) => {
 
 
   const recEngine = () => {
-    const activeUserHate = () => {
-      return jAPI.userMovieExpand("loveHates", activeId)
-        .then(userLoveHates => {
-          const lh = userLoveHates.filter(element => element.isHated === true)
-          // const hatedMovies = userLoveHates.map(el => el.movieId)
-          setActiveHate(lh)
-        })
-    };
 
-    activeUserHate();
-
-    const overallUserHate = () => {
-      return jAPI.movieExpand("loveHates")
-        .then(overallLoveHates => {
-          const olh = overallLoveHates.filter(element => element.userId !== activeId && element.isHated === true);
-          const userInCommon = []
-          olh.forEach(loveHate => {
-            if (userInCommon.filter(userObj => userObj.userId !== null).length > 1) {
-              userInCommon.push({userId: loveHate.userId, hateCount: 0})
-            }
+    return jAPI.userMovieExpand("loveHates", activeId)
+      .then(userLoveHates => {
+        const ulh = userLoveHates.filter(element => element.isHated === true)
+        console.log(ulh)
+        jAPI.movieExpand("loveHates")
+          .then(overallLoveHates => {
+            const olh = overallLoveHates.filter(element => element.userId !== activeId && element.isHated === true);
+            // console.log(olh)
+            const sameSameArr = []
+            ulh.forEach(ulhObject => {
+              for (let i = 0; i < olh.length; i++) {
+                if (ulhObject.movieId === olh[i].movieId) {
+                  sameSameArr.push(olh[i])
+                }
+              }
+              console.log(sameSameArr)
+            })
+            
           })
-          console.log("userincommon", userInCommon)
-          
-          setOverallHate(olh);
-        })
-    }
-    overallUserHate();
+      })
 
-    const sameMovies = () => {
-      let sameMovies = [];
-      for (let i = 0; i < activeHate.length; i++) {
-        const thingy = overallHate.filter(object => object.movieId === activeHate[i].movieId)
-        sameMovies = sameMovies.concat(thingy)
-      }
-      setOverallHate(sameMovies)
-    };
+    // activeUserHate();
 
-    sameMovies();
+    // const overallUserHate = () => {
+    //   return jAPI.movieExpand("loveHates")
+    //     .then(overallLoveHates => {
+    //       const olh = overallLoveHates.filter(element => element.userId !== activeId && element.isHated === true);
+    //       const userInCommon = []
+    //       olh.forEach(loveHate => {
+    //         if (userInCommon.filter(userObj => userObj.userId !== null).length > 1) {
+    //           userInCommon.push({userId: loveHate.userId, hateCount: 0})
+    //         }
+    //       })
+    //       console.log("userincommon", userInCommon)
+
+    //       setOverallHate(olh);
+    //     })
+    // }
+    // overallUserHate();
+
+    // const sameMovies = () => {
+    //   let sameMovies = [];
+    //   for (let i = 0; i < activeHate.length; i++) {
+    //     const thingy = overallHate.filter(object => object.movieId === activeHate[i].movieId)
+    //     sameMovies = sameMovies.concat(thingy)
+    //   }
+    //   setOverallHate(sameMovies)
+    // };
+
+    // sameMovies();
 
   }
   console.log(activeHate, "active");
