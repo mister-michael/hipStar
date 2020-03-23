9cbebe1b2b3005a7824bc80fb31f1fd9224b2804
Michael Clark <d.a.n.k.e.@Michaels-iPro.attlocal.net>
2020-03-23 15:11:56 -0500

    buttons need to update db, some styling done which may not work after updates

--- src/components/search/SearchCard.js
+++ src/components/search/SearchCard.js
@@ -9,20 +9,22 @@ import {
 
 const SearchCard = (props) => {
 
-  const mdbId = props.result.id
-  const activeUserId = props.activeUserId
+  const mdbId = props.result.id;
+  const activeUserId = props.activeUserId;
+  const [classNames, setClassNames] = useState({loveClass: "unlovedBtn", hateClass: "unhatedBtn"});
+  const [textColors, setTextColors] = useState({loveText: "redText", hateText: "greenText"})
 
-  let poster = "https://harperlibrary.typepad.com/.a/6a0105368f4fef970b01b8d23c71b5970c-800wi"
+  let poster = "https://harperlibrary.typepad.com/.a/6a0105368f4fef970b01b8d23c71b5970c-800wi";
 
   const imageHandler = () => {
     if (props.result.poster_path !== null) {
       return `https://image.tmdb.org/t/p/w500${props.result.poster_path}`
     } else {
       return poster
-    }
-  }
+    };
+  };
 
-  const handleAdd = () => {
+  const handleHate = () => {
 
     mAPI.searchWithId(mdbId)
       .then(movieById => {
@@ -35,12 +37,12 @@ const SearchCard = (props) => {
           revenue: movieById.revenue,
           overview: movieById.overview,
           tagline: movieById.tagline
-        }
+        };
 
         jAPI.get("movies")
           .then(movies => {
 
-            const movieInJson = movies.find(movie => movie.dbid === movieById.id)
+            const movieInJson = movies.find(movie => movie.dbid === movieById.id);
 
             if (movieInJson !== undefined) {
 
@@ -48,11 +50,11 @@ const SearchCard = (props) => {
                 userId: props.activeUserId,
                 movieId: movieInJson.id,
                 isHated: true
-              }
+              };
 
               jAPI.get("loveHates")
                 .then(loveHatesFetch => {
-                  const loveHateFound = loveHatesFetch.find(object => object.userId === activeUserId && object.movieId === movieInJson.id)
+                  const loveHateFound = loveHatesFetch.find(object => object.userId === activeUserId && object.movieId === movieInJson.id);
 
                   if (loveHateFound === undefined) {
 
@@ -61,7 +63,7 @@ const SearchCard = (props) => {
 
                     window.alert("already on your list")
                   }
-                })
+                });
 
             } else {
 
@@ -72,12 +74,73 @@ const SearchCard = (props) => {
                     userId: props.activeUserId,
                     movieId: movieObj.id,
                     isHated: true
-                  }
-                  jAPI.save(loveHateObject2, "loveHates")
+                  };
+                  jAPI.save(loveHateObject2, "loveHates");
                 })
             }
           })
+          setClassNames({loveClass: "unlovedBtn", hateClass: "hatedBtn"});
+          setTextColors({loveText: "redText", hateClass: "whiteText"});
+        props.searchInput.value = "";
+      })
+  };
+  const handleLove = () => {
 
+    mAPI.searchWithId(mdbId)
+      .then(movieById => {
+
+        const movieObject = {
+          dbid: movieById.id,
+          title: `${movieById.title}`,
+          releaseDate: movieById.release_date,
+          posterPath: imageHandler(),
+          revenue: movieById.revenue,
+          overview: movieById.overview,
+          tagline: movieById.tagline
+        };
+
+        jAPI.get("movies")
+          .then(movies => {
+
+            const movieInJson = movies.find(movie => movie.dbid === movieById.id);
+
+            if (movieInJson !== undefined) {
+
+              const loveHateObject = {
+                userId: props.activeUserId,
+                movieId: movieInJson.id,
+                isHated: true
+              };
+
+              jAPI.get("loveHates")
+                .then(loveHatesFetch => {
+                  const loveHateFound = loveHatesFetch.find(object => object.userId === activeUserId && object.movieId === movieInJson.id);
+
+                  if (loveHateFound === undefined) {
+
+                    jAPI.save(loveHateObject, "loveHates")
+                  } else {
+
+                    window.alert("already on your list")
+                  }
+                });
+
+            } else {
+
+              jAPI.save(movieObject, "movies")
+                .then(movieObj => {
+
+                  const loveHateObject2 = {
+                    userId: props.activeUserId,
+                    movieId: movieObj.id,
+                    isHated: false
+                  };
+                  jAPI.save(loveHateObject2, "loveHates")
+                });
+            }
+          })
+        setClassNames({loveClass: "lovedBtn", hateClass: "unhatedBtn"});
+        setTextColors({loveText: "whiteText", hateClass: "greenText"});
         props.searchInput.value = ""
       })
   };
@@ -85,13 +148,14 @@ const SearchCard = (props) => {
   const release = () => {
     const releaseDate = "release_date";
     if (props.result[releaseDate] !== undefined) {
-      return props.result[releaseDate].split("-")[0]
-    }
-  }
+      return props.result[releaseDate].split("-")[0];
+    };
+  };
 
   useEffect(() => {
 
-  }, [])
+  }, [classNames]);
+
   return (
     <>
 
@@ -100,8 +164,15 @@ const SearchCard = (props) => {
         <CardTitle>{props.result.title}</CardTitle>
         <CardSubtitle>{release()}</CardSubtitle>
         <CardBody className="css1">
-          <Button outline id={`button--${props.result.id}`}
-            onClick={handleAdd} color="primary">Add</Button>{' '}
+          <Button outline 
+          id={`love-button--${props.result.id}`}
+            onClick={handleLove} 
+            className={classNames.loveClass}><span className={textColors.loveText}>Love</span></Button>{' '}
+          <Button outline 
+          id={`hate-button--${props.result.id}`}
+            onClick={handleHate} 
+            className={classNames.hateClass}
+            ><span className={textColors.hateText}>Hate</span></Button>{' '}
         </CardBody>
       </div>
 
