cdd8f48835725123e2afab7cf268a1ec215a2e47
Michael Clark <michaelclarknashville@protonmail.com>
2020-03-26 21:33:31 -0500

    hpstr page styling

--- src/components/hpstr/Hpstr.js
+++ src/components/hpstr/Hpstr.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react"
 import jAPI from "../../modules/apiManager"
+import "./Hpstr.css"
 
 const Hpstr = () => {
 
-  const [movieObject, setMovieObject] = useState({})
+    const [movieObject, setMovieObject] = useState({})
 
     const movieToDisplay = "The Cat and the Canary"
 
@@ -13,22 +14,29 @@ const Hpstr = () => {
                 const theOneMovie = movies.filter(movie => movie.title.toLowerCase() === movieToDisplay.toLowerCase())
                 setMovieObject(theOneMovie[0])
             })
-        }
-    
+    }
+
     console.log(movieObject)
-    
+
     useEffect(() => {
         fetchMovie();
     }, [])
 
     return (
         <>
-            <div className="andysDiv">
-                <h1>the only movie to ever see</h1>
-                <h2>{movieObject.title}</h2>
-                <img src={movieObject.posterPath} alt="poster" />
-                <div>{movieObject.overview}</div>
-            </div>
+            <h2 className="headline">the only movie to ever see</h2>
+            <section className="andysContainer">
+                <div className="andysDiv">
+                    <div className="bodyContainer">
+                    </div>
+                    <img src={movieObject.posterPath} alt="poster"
+                        className="hpstrImage" />
+                    <div className="bodyContainer">
+                        <h5 className="hpstrMovieTitle">{movieObject.title}</h5>
+                        <div>{movieObject.overview}</div>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
